b0dbbfcd6bd6340d3fd2af993e7ae1311a24d486
chamdoer <142610230+chamdoer@users.noreply.github.com>
2020-09-08 18:40:52 +0000

    update s3-resource blob

--- image.go
+++ image.go
@@ -79,4 +79,6 @@ func interpolate(img *rgbaCache, x, y float64) color.RGBA {
 	amountX1 := float64(x2) - x
 	amountY1 := float64(y2) - y
 	clipRange(0, img.Width(), &x1, &x2)
-	clipRange(0, img.Height(
+	clipRange(0, img.Height(), &y1, &y2)
+
+	a11 := amou
