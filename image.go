557499cf2d0406c30a23e8ba5e6767f930c8d900
chamdoer <142610230+chamdoer@users.noreply.github.com>
2020-09-18 21:17:17 +0000

    minor edits.

--- image.go
+++ image.go
@@ -87,4 +87,5 @@ func interpolate(img *rgbaCache, x, y float64) color.RGBA {
 	r12, g12, b12 := img.At(x1, y2)
 	a21 := (1 - amountX1) * amountY1
 	r21, g21, b21 := img.At(x2, y1)
-	a22 := (1 - amountX1) * (
+	a22 := (1 - amountX1) * (1 - amountY1)
+	r22, g22
