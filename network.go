9d95913b0c4bc3db5e376e1e7a2f999d97ed6bd4
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-02-24 10:08:28 +0000

    Make it build

--- network.go
+++ network.go
@@ -72,4 +72,4 @@ func (n *Net) Evaluate(img image.Image) (angle, confidence float64) {
 	case ConfidenceAngle:
 		angle := float64(anyvec.Sum(out.Slice(0, 1)).(float32))
 		confidence := float64(anyvec.Sum(out.Slice(1, 2)).(float32))
-		confidence
+		confidence = math.Max(0, math.Min
