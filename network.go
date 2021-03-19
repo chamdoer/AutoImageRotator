b6936bbf93615baf147e057fdc4d8f7c50395a18
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-03-19 21:12:06 +0000

    Bumped up version.

--- network.go
+++ network.go
@@ -97,4 +97,5 @@ func (n *Net) Cost(desired, actual anydiff.Res, num int) anydiff.Res {
 			confidenceMapper := confidenceAngleMapper(1, num)
 			angles := anydiff.Map(angleMapper, actual)
 			costs := anydiff.Complement(anydiff.Cos(anydiff.Sub(angles, desired)))
-			confidences := anydiff.Map(co
+			confidences := anydiff.Map(confidenceMapper, actual)
+	
