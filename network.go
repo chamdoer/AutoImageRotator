42dc3753f9b9afe06371f4952d8703458d1296cf
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-04-26 07:32:36 +0000

    add some function

--- network.go
+++ network.go
@@ -140,4 +140,5 @@ func rightAngleOneHots(angles anyvec.Vector) anyvec.Vector {
 
 func rightAngleMaxes(softOut anyvec.Vector) (angles, probs anyvec.Vector) {
 	c := softOut.Creator()
-	stops := c.MakeNumericList([]float64{0, math.Pi / 2, math.
+	stops := c.MakeNumericList([]float64{0, math.Pi / 2, math.Pi, 3 * math.Pi / 2})
+	r
