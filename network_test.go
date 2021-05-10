9e36bd72f23057181f3288831ab23702e5ab8d87
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-05-10 14:13:01 +0000

    restructured files

--- network_test.go
+++ network_test.go
@@ -19,3 +19,34 @@ func TestNetworkCost(t *testing.T) {
 		)
 		net := &Net{OutputType: RawAngle}
 		actualCost := net.Cost(desired, actual, 9).Output().Data().([]float32)
+		expectedCost := []float32{
+			1.653643620863612,
+			0.000000000000000,
+			1.416146836547142,
+			0.459697694131860,
+			1.416146836547142,
+			1.416146836547142,
+			0.459697694131860,
+			0.459697694131860,
+			0.459697694131860,
+		}
+		for i, x := range expectedCost {
+			a := actualCost[i]
+			if math.Abs(float64(x-a)) > 1e-3 {
+				t.Errorf("output %d: should be %f but got %f", i, x, a)
+			}
+		}
+	})
+	t.Run("RightAngles", func(t *testing.T) {
+		actual := anydiff.NewConst(
+			anyvec32.MakeVectorData([]float32{
+				-0.69315, -1.38629, -2.30259, -1.89712,
+				-0.69315, -1.38629, -2.30259, -1.89712,
+			}),
+		)
+		desired := anydiff.NewConst(
+			anyvec32.MakeVectorData([]float32{5, 2}),
+		)
+		net := &Net{OutputType: RightAngles}
+		actualCost := net.Cost(desired, actual, 2).Output().Data().([]float32)
+		expectedCost := []float32{1.89712, 1.38629}
