6b68eb5657f8fbfdcbf907d0ffca4402d35d3cbf
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-05-21 21:32:51 +0000

    Add assertions

--- post_train/main.go
+++ post_train/main.go
@@ -1,3 +1,4 @@
 // Command post_train produces an *imagenet.Classifier for
 // a neural network.
-// As part of doing this, it converts batch norma
+// As part of doing this, it converts batch normalization
+// layers into af
