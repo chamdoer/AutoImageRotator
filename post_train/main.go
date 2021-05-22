bd4dbfbd39eb72f1ef235afea1113c1e69169f11
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-05-22 10:08:38 +0000

    Update networking.md

--- post_train/main.go
+++ post_train/main.go
@@ -1,4 +1,5 @@
 // Command post_train produces an *imagenet.Classifier for
 // a neural network.
 // As part of doing this, it converts batch normalization
-// layers into af
+// layers into affine transforms.
+package
