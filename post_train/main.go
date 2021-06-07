d35dd3f8b6297a2bd82d64cd8d1cbde02da2d93a
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-06-07 14:01:01 +0000

    updated;

--- post_train/main.go
+++ post_train/main.go
@@ -30,4 +30,5 @@ func main() {
 
 	flag.StringVar(&imgDir, "samples", "", "sample directory")
 	flag.StringVar(&inNet, "in", "", "input network")
-	flag.StringVar(&outNet, "out", "", "output net
+	flag.StringVar(&outNet, "out", "", "output network")
+	flag.IntVar(&batch
