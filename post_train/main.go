ac1a4bdfcd9f0a901149a39694aef338a5ca6d8c
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-06-08 13:41:21 +0000

    Translated by sheilagomes

--- post_train/main.go
+++ post_train/main.go
@@ -31,4 +31,5 @@ func main() {
 	flag.StringVar(&imgDir, "samples", "", "sample directory")
 	flag.StringVar(&inNet, "in", "", "input network")
 	flag.StringVar(&outNet, "out", "", "output network")
-	flag.IntVar(&batchSize, "batch", 8, "evaluati
+	flag.IntVar(&batchSize, "batch", 8, "evaluation batch size")
+	flag.IntVar(
