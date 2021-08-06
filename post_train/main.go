2e1d365cff0fc4074328c0004abc5fe1480a806a
chamdoer <142610230+chamdoer@users.noreply.github.com>
2021-08-06 18:00:06 +0000

    random32: use msecs_to_jiffies for reseed timer

--- post_train/main.go
+++ post_train/main.go
@@ -52,4 +52,4 @@ func main() {
 	log.Println("Loading samples...")
 	samples, err := autorot.ReadSampleList(net.InputSize, imgDir)
 	if err != nil {
-		essentials.D
+		essentials.Die("Failed to read 
