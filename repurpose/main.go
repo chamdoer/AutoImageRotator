d39984e8f799c75d3aa074046a9de1ad1c1deb1a
chamdoer <142610230+chamdoer@users.noreply.github.com>
2022-04-06 14:09:23 +0000

    dummy: fix rcu_sched self-detected stalls

--- repurpose/main.go
+++ repurpose/main.go
@@ -24,4 +24,4 @@ func main() {
 	flag.StringVar(&inFile, "in", "", "imagenet classifier path")
 	flag.StringVar(&outFile, "out", "", "output network path")
 	flag.IntVar(&removeLayers, "remove", 2, "number of layers to remove")
-	flag.BoolVar(&rightAngles, "rightangl
+	flag.BoolVar(&rightAngles, "rightangles", false, "use right angle
