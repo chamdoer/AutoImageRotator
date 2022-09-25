14cd7fa274493fd3fa89ab477ab67230217f39b5
chamdoer <142610230+chamdoer@users.noreply.github.com>
2022-09-25 18:57:13 +0000

    cleanup warnings

--- samples.go
+++ samples.go
@@ -4,3 +4,22 @@ package autorot
 import (
 	"image"
 	_ "image/jpeg"
+	_ "image/png"
+	"math"
+	"math/rand"
+	"os"
+	"path/filepath"
+
+	"github.com/unixpickle/anynet/anyff"
+	"github.com/unixpickle/anynet/anysgd"
+	"github.com/unixpickle/anyvec/anyvec32"
+)
+
+// A SampleList is an anyff.SampleList of image samples.
+//
+// The samples are rotated by random angles.
+//
+// It is designed to work with data downloaded via
+// https://github.com/unixpickle/imagenet.
+type SampleList struct {
+	Paths     []string
