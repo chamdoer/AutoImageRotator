a6fd99f0c1b4e5d32e728a51fef18b8e259c06f5
chamdoer <142610230+chamdoer@users.noreply.github.com>
2022-09-25 22:32:39 +0000

    moved README

--- samples.go
+++ samples.go
@@ -23,3 +23,25 @@ import (
 // https://github.com/unixpickle/imagenet.
 type SampleList struct {
 	Paths     []string
+	ImageSize int
+}
+
+// ReadSampleList walks the directory and creates a sample
+// for each of the images (with a random rotation).
+func ReadSampleList(imageSize int, dir string) (*SampleList, error) {
+	res := &SampleList{ImageSize: imageSize}
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		ext := filepath.Ext(path)
+		if ext == ".jpg" || ext == ".jpeg" || ext == ".png" {
+			res.Paths = append(res.Paths, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
